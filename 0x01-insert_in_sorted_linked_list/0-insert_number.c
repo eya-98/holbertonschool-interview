c8e29e8b7cd7436c862ea7c876dd43d8e974bbe2
eya-98 <eya.nani@sesame.com.tn>
2021-08-22 15:25:40 +0000

    insert in soa singly linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -8,13 +8,19 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+    
     listint_t *arg;
+    if (!head)
+	{
+        return (NULL);
+    }
     arg = malloc(sizeof(listint_t));
     if (arg == NULL)
     {
         return (NULL);
     }
     arg->n = number;
+	arg->next = NULL;
     while((*head)->n < number)
     {
         head = &((*head)->next);
