40346df3b0489967252e3d0a355936f0a49926cc
eya-98 <eya.nani@sesame.com.tn>
2021-08-22 14:01:25 +0000

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """a method that determines if all the boxes can be opened."""
+
+
 def canUnlockAll(boxes):
-    newlist=[];
-    index = 0;
-    emptiness = 0;
+    newlist = []
+    index = 0
+    emptiness = 0
     for i in boxes:
         if (i == []):
-            emptiness+=1;
+            emptiness += 1
         if emptiness > 1:
-            return False;
+            return False
     if emptiness == 1:
-        index+=1;
+        index += 1
         newlist.append([])
     for j in boxes:
         for i in boxes:
             if (index in i):
                 if (i in newlist):
-                    continue;
+                    continue
                 else:
-                    newlist.append(i);
-                    index+=1;
-                    break;
+                    newlist.append(i)
+                    index += 1
+                    break
     if (len(newlist) == len(boxes)):
         return (True)
     else:
