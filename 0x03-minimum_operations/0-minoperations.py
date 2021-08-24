a25c760f52c40e43c21c3134c49f158cc98ade5a
eya-98 <eya.nani@sesame.com.tn>
2021-08-24 15:41:23 +0000

    minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def prem(n):
+    for i in range(2, n):
+        if (n % i == 0):
+            return(False)
+    return(True)
+
+
+def minOperations(n):
+    i = 2
+    opp = 0
+    while(n != 1):
+        if prem(i):
+            if (n % i == 0):
+                n = n / i
+                opp = opp + i
+            else:
+                i += 1
+        else:
+            i += 1
+    return(opp)
