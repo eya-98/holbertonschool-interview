79cd4559090a5a36c294e8422e0a889dca69c1b6
eya-98 <eya.nani@sesame.com.tn>
2022-01-18 14:03:37 +0100

    AVL

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,63 @@
+#include "binary_trees.h"
+
+/**
+ * insert_into_avl - Inserts an element in the tree
+ * @head: The head
+ * @value: new element to insert
+ * Return: The new node
+ **/
+avl_t *insertElement(avl_t *head, int value)
+{
+	avl_t *NewElement = NULL;
+
+	NewElement = malloc(sizeof(avl_t));
+	if (NewElement == NULL)
+		return (NULL);
+	NewElement->n = value;
+	NewElement->left = NULL;
+	NewElement->right = NULL;
+	NewElement->parent = head;
+
+	return (NewElement);
+}
+
+/**
+ * insert_start_end - Recursive fn Used to convert an array to AVL
+ * @start: Head / from where to start
+ * @array: array to convert to AVL
+ * @first: Position where to start adding element
+ * @last: Position where to stop adding element
+ *
+ * Return: Head of the AVL
+ **/
+avl_t *insert_start_end(avl_t *start, int *array, int first, int last)
+{
+	int middle;
+	avl_t *head;
+
+	if (first > last)
+		return (NULL);
+
+	middle = (first + last) / 2;
+	head = insertElement(start, array[middle]);
+
+	if (head == NULL)
+		return (NULL);
+
+	head->left = insert_start_end(head, array, first, middle - 1);
+	head->right = insert_start_end(head, array, middle + 1, last);
+
+	return (head);
+}
+
+/**
+ * sorted_array_to_avl - Converts an array to AVL
+ * @array: Array to convert
+ * @size: Number of elements in the array
+ *
+ * Return: head of the AVL
+ **/
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	return (insert_start_end(NULL, array, 0, size - 1));
+}
