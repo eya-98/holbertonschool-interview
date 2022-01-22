c83d5f101acb627408a3b425e0b2377294620ffd
eya-98 <eya.nani@sesame.com.tn>
2022-01-22 13:39:24 +0100

    header file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,9 @@
+#ifndef holberton_h
+#define holberton_h
+#include <stdlib.h>
+int _putchar(char c);
+int _strlen(char *str);
+void printerror(void);
+int _isDigit(char c);
+void multiply(char *nb, char *nb2, int nblength, int nb2length);
+#endif
