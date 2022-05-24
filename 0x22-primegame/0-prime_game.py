d2c3ca236a8083ccac05fd45a73cf45da09130d8
eya-98 <eya.nani@sesame.com.tn>
2022-05-24 12:19:48 +0100

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -9,17 +9,16 @@ def isWinner(x, nums):
     if (len(nums) >= 2):
         for i in range(x):
             game = 0
-            print(nums)
-            newList = list()
+            # newList = list()
+            number = nums[0]
+            player += 1
             for num in nums:
-                player += 1
-                number = nums[0]
                 if (num % number == 0):
                     nums.remove(num)
                     game += 1
             if (game == 0) or (len(nums) == 1):
                 if (player % 2 == 0):
-                    return 'Maria'
+                    return 'Maria '
                 else:
-                    return 'Ben'
+                    return 'Ben '
     return None
