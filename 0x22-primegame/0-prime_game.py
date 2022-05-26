e2e0a4dc2c91ea49f6230fe3d0d497af88b52743
eya-98 <eya.nani@sesame.com.tn>
2022-05-26 15:59:51 +0100

    prime game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python3
 """ Prime game """
+def checkprime(n):
+    """check if the number is prime"""
+    prime = 0
+    if (n == 1):
+        return True
+    if (n == 0):
+        return False
+    for i in range(2, n):
+        if (n % i) == 0:
+            prime += 1
+    if prime != 0:
+        return False
+    else:
+        return True
+
+def multiple(lists, n):
+    """delete all the multiples of a number in a list"""
+    newlist = lists.copy()
+    for i in lists:
+        if (i % n == 0):
+            newlist.remove(i)
+    return newlist
 
 
 def isWinner(x, nums):
     """ return the name of the winner of the game"""
     player = -1
     nums = sorted(nums, reverse=True)
-    if (len(nums) >= 2):
+    if len(nums) > 1:
         for i in range(x):
             game = 0
             newList = nums.copy()
-            number = nums[0]
             player += 1
             for num in nums:
-                if(len(nums) > 1 and num != 0):
-                    if (num % number == 0) :
-                        game += 1
-                        newList.remove(num)
+                if(checkprime(num) == True):
+                    if (num == 1):
+                        newList.remove(1)
+                    else:
+                        newList = multiple(nums, num) 
+                    game += 1
+                    break
+                else:
+                    continue
             nums = newList.copy()
             if (game == 0):
                 if (player % 2 == 0):
                     return 'Ben'
-                else:   
+                else: 
                     return 'Maria'
-            if (game != 0):
-                if (len(nums) == 1):
-                    if (player % 2 == 0):
-                        return 'Maria'
-                    else:
-                        return 'Ben'
-                if (len(nums) == 0):
-                    if (player % 2 == 0):
-                        return 'Ben'
-                    else:
-                        return 'Maria'
-                else:
-                    continue
-
-    return None
+        return None
