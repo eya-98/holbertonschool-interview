a5d1c3feb64b198d55dff776c025c8492249b7d7
eya-98 <eya.nani@sesame.com.tn>
2022-05-27 17:52:04 +0100

    edit

--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -1,5 +1,7 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 #endif
